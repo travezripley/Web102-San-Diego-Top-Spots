f2e6b4069fb86d00e35cf7c986a94a781820d267
Travezripley <theripleys@gmail.com>
2019-03-04 12:24:06 -0800

    adds initial project files

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-$(document).ready(function() {
-// write your code here
+//San Diego Top Spots *main.js* 
+//Created by Travis Ripley on 03/02/2019 for SDCS/Web102
 
-});
+$(document).ready(function () {
+    //Get Top Spots data using $.getJSON method
+    $.getJSON("data.json", function (data) {
+    
+            for (let i = 0; i < data.length; i++) {
+                let longitude = data[i].location[0];
+                let latitude = data[i].location[1];
+                
+                /** Map Data */
+                let mapLink = 'https://www.google.com/maps?q=' + longitude + ',' + latitude;
+            
+
+            /** This pushes data into a table **/
+            let trHtml = "<tr><td>" + data[i].name + "</td><td>" + data[i].description + "</td><td>" + "<a href =" + mapLink + "><button> Open in Google Maps </button></a></td></tr>";
+            $(trHtml).appendTo("#topSpots"); 
+ 
+
+            console.log(data, longitude, latitude, mapLink);
+            } 
+    });
+
+})
---
+++ readme.md
@@ -0,0 +1,16 @@
+March 4th, 2019
+
+Hey guys, thanks for Checking out my code in my Github.
+
+What you see here is a "San Diego Top Spots" webapp.
+This is what happens, Upon loading the app, a table is populated with data from a JSON file, in turn that lists a description, name and location (using Google Maps) of popular tourist spots around San Diego to visit.
+
+To get started, open the app in VS code, or code editor of your choosing, and install the dependencies via the terminal by typing '$ npm install'. Next, in the terminal, type '$ static .' to start the app on the local node server and open the localhost:8080 in the browser of your choice.
+
+Please Comment, Like and Subscribe to my Youtube Channel. 
+Tell your Friends,
+Your Programming Pal,
+-Travez
+
+Theripleys@gmail.com
+https://www.youtube.com/c/TravisRipley
