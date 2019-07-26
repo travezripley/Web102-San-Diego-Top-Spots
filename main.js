852872434364dd9452701c1588ede7465865e3fa
Travis Ripley <Theripleys@gmail.com>
2019-07-25 18:55:42 -0700

    adds updates to index.html, package.json, main.js

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
     $.getJSON("data.json", function(response) {
-      $.each(response, function(i, item) {
+    $.each(response, function(i, item) {
         let longitude = item.location[0];
         let latitude = item.location[1];
         var $tr = $("<tr>").append(
-          $("<td>").text(item.name),
-          $("<td>").text(item.description),
-          $("<td>").append(
+        $("<td>").text(item.name),
+        $("<td>").text(item.description),
+        $("<td>").append(
             `<a class='btn btn-primary btn-sm' href='https://www.google.com/maps?q=${longitude},${latitude}'>Google Maps</a>`
-          )
+        )
         );
         $tr.appendTo(".table");
-      });
     });
-  });
-  
+    });
+});
