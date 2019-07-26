f877e776aa27c8a7c9f5a057f53093618072ad4a
Travis Ripley <Theripleys@gmail.com>
2019-07-25 18:52:41 -0700

    adds updates to index.html, package.json, main.js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-$(document).ready(function () {
-    //Get Top Spots data using $.getJSON method
-    $.getJSON("data.json", function (data) {
-            for (let i = 0; i < data.length; i++) {
-                let longitude = data[i].location[0];
-                let latitude = data[i].location[1];
-                /** Map Data */
-                let mapLink = 'https://www.google.com/maps?q=' + longitude + ',' + latitude;
-            
-            /** This pushes data into a table **/
-            let trHtml = "<tr><td>" + data[i].name + "</td><td>" + data[i].description + "</td><td>" + "<a href =" + mapLink + "><button> Open in Google Maps </button></a></td></tr>";
-            $(trHtml).appendTo("#topSpots"); 
-            console.log(data, longitude, latitude, mapLink);
-            } 
+$(document).ready(function() {
+    $.getJSON("data.json", function(response) {
+      $.each(response, function(i, item) {
+        let longitude = item.location[0];
+        let latitude = item.location[1];
+        var $tr = $("<tr>").append(
+          $("<td>").text(item.name),
+          $("<td>").text(item.description),
+          $("<td>").append(
+            `<a class='btn btn-primary btn-sm' href='https://www.google.com/maps?q=${longitude},${latitude}'>Google Maps</a>`
+          )
+        );
+        $tr.appendTo(".table");
+      });
     });
-
-})
+  });
+  
